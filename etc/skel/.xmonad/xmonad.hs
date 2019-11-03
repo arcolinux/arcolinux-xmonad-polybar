0c337250b687f2f1c2e7a6ee4fbb880e21c3bb30
Erik Dubois <erik.dubois@gmail.com>
2019-11-03 10:34:16 +0100

    alt + k slimlock

--- etc/skel/.xmonad/xmonad.hs
+++ etc/skel/.xmonad/xmonad.hs
@@ -204,6 +204,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   , ((mod1Mask, xK_Down), spawn $ "variety --resume" )
   , ((mod1Mask, xK_Left), spawn $ "variety -p" )
   , ((mod1Mask, xK_Right), spawn $ "variety -n" )
+  , ((mod1Mask, xK_k), spawn $ "slimlock" )
   , ((mod1Mask, xK_F2), spawn $ "gmrun" )
   , ((mod1Mask, xK_F3), spawn $ "xfce4-appfinder" )
 
