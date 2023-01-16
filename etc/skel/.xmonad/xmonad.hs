cea8c4089f4f79d16f64975e45e56651330c4609
arcolinuxz <arcolinuxinfo@gmail.com>
2023-01-16 08:42:00 +0100

    rofi

--- etc/skel/.xmonad/xmonad.hs
+++ etc/skel/.xmonad/xmonad.hs
@@ -165,6 +165,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
 
   , ((modMask .|. shiftMask , xK_Return ), spawn $ "thunar")
   , ((modMask .|. shiftMask , xK_d ), spawn $ "dmenu_run -i -nb '#191919' -nf '#fea63c' -sb '#fea63c' -sf '#191919' -fn 'NotoMonoRegular:bold:pixelsize=14'")
+  , ((modMask, xK_d ), spawn $ "~/.xmonad/launcher/launcher.sh")
   , ((modMask .|. shiftMask , xK_r ), spawn $ "xmonad --recompile && xmonad --restart")
   , ((modMask .|. shiftMask , xK_q ), kill)
   -- , ((modMask .|. shiftMask , xK_x ), io (exitWith ExitSuccess))
